ed7f62a5be038f4c0820d665fe8b673ef0a053d6
Cameron Stone <226145444+Cameron-Stone-acs@users.noreply.github.com>
2026-01-29 13:10:17 -0800

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 10f;
     public float boostMult = 2f;
     public float launchMult = 10f;
+    public float launchMinSpeed = 0.5f;
     public float airBoostMult = 5f;
     public float boostTime = 10f;
     public float boostRechargeRate = 0.1f;
@@ -21,9 +22,8 @@ public class PlayerController : MonoBehaviour
     private bool delay = false;
     private bool airBoostUsed = false;
     private bool launched = false;
-    private float Horizontal;
-    private float Vertical;
-    
+    public float hVelocity;
+    public float vVelocity;
     void Start()
     {
         gd = gdobject.GetComponent<GroundCheck>();
@@ -31,33 +31,32 @@ public class PlayerController : MonoBehaviour
     }
     void LateUpdate()
     {
+        hVelocity = Mathf.Abs(rb.velocity.x + rb.velocity.z);
+        vVelocity = Mathf.Abs(rb.velocity.y);
+        float Horizontal = Input.GetAxis("Horizontal") * Speed * Time.fixedDeltaTime;
+        float Vertical = Input.GetAxis("Vertical") * Speed * Time.fixedDeltaTime;
         //fix launch boost when going in a negative direction
-        if (Input.GetAxis("Boost") <= 0 && gd.isGrounded && launched && rb.velocity.x + rb.velocity.z < 0.3f) launched = false;
-        Debug.Log("Horizontal Velocity: " + rb.velocity.x + rb.velocity.z);
+        if (Input.GetAxis("Boost") <= 0 && gd.isGrounded && launched && hVelocity < launchMinSpeed) launched = false;
+        Debug.Log("Horizontal Velocity: " + hVelocity);
         if (airBoostUsed && gd.isGrounded) airBoostUsed = false;
         if (Mathf.Lerp(90, rb.velocity.magnitude * 10, 0.1f) > 145) Camera.main.fieldOfView = 145;
         else Camera.main.fieldOfView = Mathf.Lerp(90, rb.velocity.magnitude * 10, 0.1f);
         if (boostCharge <= 0) delay = true;
         if (boostCharge >= boostTime && delay) delay = false;
-        if (Input.GetAxis("Boost") > 0 && boostCharge > 0 && gd.isGrounded && !delay && rb.velocity.x + rb.velocity.z > 0.4f)
+        if (Input.GetAxis("Boost") > 0 && boostCharge > 0 && gd.isGrounded && !delay && hVelocity > launchMinSpeed)
         {
-            Horizontal = Input.GetAxis("Horizontal") * Speed * Time.fixedDeltaTime * boostMult;
-            Vertical = Input.GetAxis("Vertical") * Speed * Time.fixedDeltaTime * boostMult;
+            Horizontal = Horizontal * boostMult;
+            Vertical = Vertical * boostMult;
             boostCharge -= Time.fixedDeltaTime * boostDischargeRate;
         }
-        else if (Input.GetAxis("Boost") > 0 && rb.velocity.x + rb.velocity.z < 0.4f && gd.isGrounded && !delay && !launched && boostCharge >= boostTime/10)
+        else if (Input.GetAxis("Boost") > 0 && hVelocity < launchMinSpeed && gd.isGrounded && !delay && !launched && boostCharge >= boostTime/10)
         {
             Vertical = 1 * Speed * Time.fixedDeltaTime * launchMult;
             Debug.Log("Launch");
             boostCharge -= boostTime/10;
             launched = true;
         }
-        else 
-        {
-            Horizontal = Input.GetAxis("Horizontal") * Speed * Time.fixedDeltaTime;
-            Vertical = Input.GetAxis("Vertical") * Speed * Time.fixedDeltaTime;
-            if (boostCharge < boostTime) boostCharge += Time.fixedDeltaTime * boostRechargeRate;
-        }
+        else if (boostCharge < boostTime) boostCharge += Time.fixedDeltaTime * boostRechargeRate;
         Vector3 Movement = Cam.transform.right * Horizontal + Cam.transform.forward * Vertical;
         Movement.y = 0f;
         if (Input.GetAxis("Boost") > 0 && Input.GetAxis("Jump") <= 0 && !gd.isGrounded && boostCharge >= boostTime / 2 && !airBoostUsed && !delay)
